aa0fa8dbaf63f4e2bb0fd030f8874a447b12640e
Olivier Rafalo <olivier.rafalo@infotech.edu.pl>
2026-05-20 09:42:29 +0200

    First commit

---
+++ rpg.py
@@ -0,0 +1,15 @@
+import random
+
+def generuj_postac():
+    postac = {
+        "imie": random.choice(["Legolas", "Aragorn", "Gandalf", "Frodo"]),
+        "rasa": random.choice(["Człowiek", "Czarodziej", "Elf", "Hobbit"]),
+ # TU WPISZ STATYSTYKI 
+}
+    
+    print("TWOJA POSTAĆ RPG")
+    for klucz, wartosc in postac.items():
+        print(f"{klucz.capitalize()}: {wartosc}")
+
+if __name__ == "__main__":
+    generuj_postac()
