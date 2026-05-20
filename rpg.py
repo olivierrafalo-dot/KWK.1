25d3a1e008134dc21ed2e22186ce47c07c5e4716
Olivier Rafalo <olivier.rafalo@infotech.edu.pl>
2026-05-20 10:15:19 +0200

    Fix merge conflict and combine all RPG stats

--- rpg.py
+++ rpg.py
@@ -4,20 +4,10 @@ def generuj_postac():
     postac = {
         "imie": random.choice(["Legolas", "Aragorn", "Gandalf", "Frodo"]),
         "rasa": random.choice(["Człowiek", "Czarodziej", "Elf", "Hobbit"]),
-	"sila":random.randint(10,20),"zrecznosc":random.randint5,15)
-}
-
-def pokaz_bohatera():
-    print("--- TWOJA POSTAĆ RPG ---")
-    print("Siła: 15")  # <-- To jest Twoja dopisana linijka
-    print("Klasa: Mag")
-
-if __name__ == "__main__":
-    pokaz_bohatera()
+        "sila": random.randint(10, 20), "zrecznosc": random.randint(5, 15),
+        "mana": random.randint(50, 100), "inteligencja": random.randint(12, 18),
+    }
     
     print("TWOJA POSTAĆ RPG")
     for klucz, wartosc in postac.items():
         print(f"{klucz.capitalize()}: {wartosc}")
-
-if __name__ == "__main__":
-    generuj_postac()
